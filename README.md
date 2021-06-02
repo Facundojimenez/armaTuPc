6bf3fa36e3212d43b88bcfc42dd533bc3c4acda6
Facundo Jimenez <facundo.gabriel.jimenez@gmail.com>
2021-06-02 20:42:34 -0300

    cambios hasta el desafio de la clase 4

---
+++ README.md
@@ -0,0 +1,3 @@
+Este proyecto se trata de un simulador de compra de productos informáticos.
+La idea es poder implementar en un futuro alguna lógica para comprar componentes que son compatibles entre si. (algo asi como la sección "Armar PC" de algunos sitios web de este rubro).
+Tambien voy a agregar alguna tabla con los productos y las cantidades para poder calcular el total abajo de todo.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("corriendo script...");
+function calcularImporteTotal(){
+    let precioProducto,
+        cantidad,
+        importeTotal = 0;
+    
+    precioProducto = parseFloat(prompt("Ingrese el precio del producto: (escriba un numero menor o igual a cero para terminar)"));
+    while(precioProducto > 0){
+        cantidad = parseInt(prompt("Ingrese la cantidad de unidades: "));
+        importeTotal += sumarProducto(precioProducto, cantidad);
+        precioProducto = parseFloat(prompt("Ingrese el precio de otro producto: (escriba 0 para terminar)"));
+    }
+    return importeTotal;
+}
+
+function sumarProducto(precio, cantidad){
+    return precio * cantidad;
+}
+
+
+//Invocacion de funciones:
+let importeTotal = calcularImporteTotal();
+alert("El importe total es: " + importeTotal);
