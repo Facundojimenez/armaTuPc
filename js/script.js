ca03be4018ad1cf2f1d71400980064ca4f378bdc
Facundo Jimenez <facundo.gabriel.jimenez@gmail.com>
2021-06-09 20:38:23 -0300

    primera entrega tp

--- js/script.js
+++ js/script.js
@@ -1,82 +1,5 @@
-
-/* ---Desafio anterior (clase 4)---
-
-
-function calcularImporteTotal(){
-    let precioProducto,
-        cantidad,
-        importeTotal = 0;
-    
-    precioProducto = parseFloat(prompt("Ingrese el precio del producto: (escriba un numero menor o igual a cero para terminar)"));
-    while(precioProducto > 0){
-        cantidad = parseInt(prompt("Ingrese la cantidad de unidades: "));
-        importeTotal += sumarProducto(precioProducto, cantidad);
-        precioProducto = parseFloat(prompt("Ingrese el precio de otro producto: (escriba 0 para terminar)"));
-    }
-    return importeTotal;
-}
-
-function sumarProducto(precio, cantidad){
-    return precio * cantidad;
-}
-
-//Invocacion de funciones:
-let importeTotal = calcularImporteTotal();
-alert("El importe total es: " + importeTotal);
-*/
-
-
-
-class Producto{
-    constructor(id, marca, modelo, descripcion, precio, categoria){
-        this.id = id;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.categoria = categoria;
-    }
-    mostrar(){
-        for(const atributo in this){
-            alert(atributo + ": " + this[atributo] + "");
-        }
-    }
-    mostrarConsole(){
-        for(const atributo in this){
-            console.log(atributo + ": " + this[atributo]);
-        }
-        console.log("\n\n");
-    }
-    cargar(){
-        this.id = parseInt(prompt("Ingrese el ID del producto"));
-        this.marca = prompt("Ingrese la marca");
-        this.modelo = prompt("Ingrese el modelo");
-        this.descripcion = prompt("Ingrese la descripcion");
-        this.precio = parseInt(prompt("Ingrese el precio"));
-        this.categoria = prompt("Ingrese la categoria");
-    }
-}
-/* Invocacion de funciones para el desafio de la clase 5
-
-const producto = new Producto(1, "intel", "i5 10400", "6 cores", 9000, "cpu");
-//producto.mostrar();
-producto.cargar();
-producto.mostrar();
-*/
-
-/*
-let arrNumeros = [1,2,3,4,5],
-    arrMenorA3 = arrNumeros.filter((elemento) => elemento < 3);
-    arrNumerosDoble = arrNumeros.map((elemento) => elemento * 2);
-
-console.log(arrNumeros.toString());
-console.log(arrMenorA3.toString());
-console.log(arrNumerosDoble.toString());
-*/
-
-
 //Desafio Complementario Clase 6: Ordenar array de productos (con bubble sort)
-
+/*
 function bubbleSort(arr, funcComparacion){
     let i,
         j;
@@ -128,50 +51,81 @@ function compararProductosMarcaAsc(prod1, prod2){
         return -1;
     }
     return 0;
+}*/
+
+class Producto{
+    constructor(id, marca, modelo, descripcion, precio, categoria){
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+    mostrar(){
+        for(const atributo in this){
+            alert(atributo + ": " + this[atributo] + "");
+        }
+    }
+    mostrarConsole(){
+        for(const atributo in this){
+            console.log(atributo + ": " + this[atributo]);
+        }
+        console.log("\n\n");
+    }
+    cargar(){
+        this.id = parseInt(prompt("Ingrese el ID del producto"));
+        this.marca = prompt("Ingrese la marca");
+        this.modelo = prompt("Ingrese el modelo");
+        this.descripcion = prompt("Ingrese la descripcion");
+        this.precio = parseInt(prompt("Ingrese el precio"));
+        this.categoria = prompt("Ingrese la categoria");
+    }
+    toString(){
+        return this.id + " " + this.marca + " " + this.modelo + " " + this.descripcion + " " + this.precio + " " + this.categoria;
+    }
 }
 
-// Llamado a las funciones, primero cargo un array con productos --> lo muestro -> Lo ordeno por marca --> lo muestro --> Lo ordeno por ID --> lo muestro
-/*
-let arrProductos = [new Producto(1,"Intel", "i3 10100"), new Producto(5, "AMD", "Ryzen 5 3600"), new Producto(2, "Intel", "i7 10700F"), new Producto(20, "AMD", "Ryzen 3 1200")];
 
-console.log("\n\n--------Array de productos original--------\n\n");
-for(producto of arrProductos){
-    producto.mostrarConsole();
-}
-
-bubbleSort(arrProductos, compararProductosMarcaAsc);
-console.log("\n\n--------Ahora lo muestro ordenado por marca ASC--------\n\n");
-for(producto of arrProductos){
-    producto.mostrarConsole();
-}
-
-
-bubbleSort(arrProductos, compararProductosIdAsc);
-console.log("\n\n--------Ahora lo muestro ordenado por id ASC-------\n\n");
-for(producto of arrProductos){
-    producto.mostrarConsole();
-}
+/*El usuario ingresa el id del producto a comprar, luego ese ID se utiliza para buscar al producto en el array  obtener su info
+Luego se solicita la cantidad y se hace la cuenta de precio*cantidad
+Por ultimo se pregunta por alert() si se desea comprar otro producto. Como resultado la suma de precio y cantidad de todos los productos.
+(se valida el ingreso del ID y de la cantidad con 'while')
 */
 
-// Desafío obligatorio de la clase 6:
-
-
-
-let arrProductos = [new Producto(1,"Intel", "i3 10100", "4 cores", 14999, "cpu"),
+function simularCompra(){
+    let arrProductos = [new Producto(1,"Intel", "i3 10100", "4 cores", 14999, "cpu"),
                     new Producto(5, "AMD", "Ryzen 5 3600", "6 cores", 31999, "cpu"),
                     new Producto(2, "Intel", "i7 10700F", "8 cores", 40000, "cpu"),
                     new Producto(20, "AMD", "Ryzen 3 1200", "4 cores", 10000, "cpu")];
+    let idIngresado,
+        cantidadIngresada,
+        stringProductos = "",
+        importeTotal = 0,
+        productoEncontrado;
+    
+    ///Concateno en una string el array de productos
+    for(producto of arrProductos){
+        stringProductos += producto.toString() + "\n";
+    }
 
-console.log("Mostrando array original con los productos");
-for(producto of arrProductos){
-    producto.mostrarConsole();
- }
+    ///Ingreso de productos y cantidad
+    do{
+        idIngresado = parseInt(prompt("Nuestros productos son: \n" + stringProductos + "\nIngrese el id del producto que desea comprar"));
+        productoEncontrado = arrProductos.find((producto) => producto.id === idIngresado);
+        while(productoEncontrado === undefined){
+            idIngresado = parseInt(prompt("No se encontró el producto ingresado, escriba un ID válido"));
+            productoEncontrado = arrProductos.find((producto) => producto.id === idIngresado);
+        }
 
-
-
-let arrProductosPrecioMasDe30000 = arrProductos.filter((producto) => producto.precio > 30000);
-
-console.log("\n\nMostrando array con los productos que tienen un precio mayor a 30000$");
-for(producto of arrProductosPrecioMasDe30000){
-    producto.mostrarConsole();
+        cantidadIngresada = parseInt(prompt("Ingrese la cantidad de unidades a comprar de ese producto"));
+        while(cantidadIngresada < 0){
+            cantidadIngresada = parseInt(prompt("La cantidad tiene que ser mayor o igual a cero, ingrese un número valido"));
+        }
+        importeTotal += productoEncontrado.precio * cantidadIngresada;
+    }while(prompt("¿Desea comprar otro producto? (s/n): ").toUpperCase() === 'S');
+    
+    alert("El importe total es: " + importeTotal);
 }
+
+simularCompra();
