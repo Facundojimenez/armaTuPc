46e0d623acb1a59de289f0247de690beb943b624
Facundo Jimenez <facundo.gabriel.jimenez@gmail.com>
2021-06-18 14:59:00 -0300

    desafio clase 8. DOM

--- js/script.js
+++ js/script.js
@@ -1,113 +1,58 @@
 class Producto{
-    constructor(id, marca, modelo, descripcion, precio, categoria){
+    constructor(id, marca, modelo, descripcion, precio, categoria, urlFoto){
         this.id = id;
         this.marca = marca;
         this.modelo = modelo;
         this.descripcion = descripcion;
         this.precio = precio;
         this.categoria = categoria;
-    }
-    mostrar(){
-        for(const atributo in this){
-            alert(atributo + ": " + this[atributo] + "");
-        }
-    }
-    mostrarConsole(){
-        for(const atributo in this){
-            console.log(atributo + ": " + this[atributo]);
-        }
-        console.log("\n\n");
-    }
-    cargar(){
-        this.id = parseInt(prompt("Ingrese el ID del producto"));
-        this.marca = prompt("Ingrese la marca");
-        this.modelo = prompt("Ingrese el modelo");
-        this.descripcion = prompt("Ingrese la descripcion");
-        this.precio = parseInt(prompt("Ingrese el precio"));
-        this.categoria = prompt("Ingrese la categoria");
-    }
-    toString(){
-        return this.id + " " + this.marca + " " + this.modelo + " " + this.descripcion + " " + this.precio + " " + this.categoria;
+        this.urlFoto = urlFoto;
     }
 }
 
-class LineaProducto{
-    constructor(id, producto, cantidad){
-        this.id = id;
-        this.producto = producto;
+class LineaProducto{ //representa una entidad que incluye una referencia al producto y su cantidad
+    constructor(idProducto, cantidad){
+        this.idProducto = idProducto;
         this.cantidad = cantidad;
     }
-    mostrarConsole(){
-        console.log("id: " + this.id);
-        console.log("producto: " + this.producto.toString());
-        console.log("cantidad: " + this.cantidad);
-    }
 }
-
-// let producto1 = new Producto(1, "Intel", "i7 10700", "8 cores", 40000, "cpus");
-// let linea1 = new LineaProducto(1, producto1, 3);
-// producto1.precio = 50000;
-// linea1.mostrarConsole();
-
-
-/*El usuario ingresa el id del producto a comprar, luego ese ID se utiliza para buscar al producto en el array  obtener su info
-Luego se solicita la cantidad y se hace la cuenta de precio*cantidad
-Por ultimo se pregunta por alert() si se desea comprar otro producto. Como resultado la suma de precio y cantidad de todos los productos.
-(se valida el ingreso del ID y de la cantidad con 'while')
-*/
-
-function simularCompra(){
-    let arrProductos = [new Producto(1,"Intel", "i3 10100", "4 cores", 14999, "cpu"),
-                    new Producto(5, "AMD", "Ryzen 5 3600", "6 cores", 31999, "cpu"),
-                    new Producto(2, "Intel", "i7 10700F", "8 cores", 40000, "cpu"),
-                    new Producto(20, "AMD", "Ryzen 3 1200", "4 cores", 10000, "cpu")];
-    let idIngresado,
-        cantidadIngresada,
-        stringProductos = "",
-        importeTotal = 0,
-        productoEncontrado;
-    
-    ///Concateno en una string el array de productos
-    for(producto of arrProductos){
-        stringProductos += producto.toString() + "\n";
-    }
-
-    ///Ingreso de productos y cantidad
-    do{
-        idIngresado = parseInt(prompt("Nuestros productos son: \n" + stringProductos + "\nIngrese el id del producto que desea comprar"));
-        productoEncontrado = arrProductos.find((producto) => producto.id === idIngresado);
-        while(productoEncontrado === undefined){
-            idIngresado = parseInt(prompt("No se encontró el producto ingresado, escriba un ID válido"));
-            productoEncontrado = arrProductos.find((producto) => producto.id === idIngresado);
-        }
-
-        cantidadIngresada = parseInt(prompt("Ingrese la cantidad de unidades a comprar de ese producto"));
-        while(cantidadIngresada < 0){
-            cantidadIngresada = parseInt(prompt("La cantidad tiene que ser mayor o igual a cero, ingrese un número valido"));
-        }
-        importeTotal += productoEncontrado.precio * cantidadIngresada;
-    }while(prompt("¿Desea comprar otro producto? (s/n): ").toUpperCase() === 'S');
-    
-    alert("El importe total es: " + importeTotal);
-}
-
-// simularCompra();
 
 // Practica de clase 8: DOM
 
+let arrProductos = [
+    new Producto(1,"Intel", "i3 10100", "Microprocesador de 4 núcleo y 8 hilos", 14999, "cpu", "media/productos/procesadores/intel-i3-10th.jpg"),         
+    new Producto(2, "Intel", "i5 10400", "Microprocesador de 6 núcleo y 12 hilos", 22999, "cpu", "media/productos/procesadores/intel-i5-10th.jpg"),
+    new Producto(3, "AMD", "Ryzen 5 3600", "Microprocesador de 6 núcleo y 12 hilos", 31999, "cpu", "media/productos/procesadores/ryzen-5.jpg"),      
+    new Producto(4, "AMD", "Ryzen 7 3700", "Microprocesador de 8 núcleo y 12 hilos", 43999, "cpu", "media/productos/procesadores/ryzen-7.jpg"),      
+    new Producto(5, "MSI", "A320m", "Motherboard compatible con los procesadores de AMD de 1era, 2da y 3ra generación", 8499, "mobo", "media/productos/mobos/amd-a320.jpg"),
+    new Producto(6, "MSI", "X570", "Motherboard compatible con los procesadores de AMD de 2da, 3ra y 4ta generación", 25399, "mobo", "media/productos/mobos/amd-x570.png"),
+    new Producto(7, "Asus", "H410m", "Motherboard compatible con los procesadores de Intel de 10va y 11va generación", 7999, "mobo", "media/productos/mobos/intel-h410.jpg"),
+    new Producto(8, "Gigabyte", "b460m", "Motherboard compatible con los procesadores de Intel de 10va y 11va generación", 14999, "mobo", "media/productos/mobos/intel-b460.jpg"),
+    new Producto(10, "Gigabyte", "RTX 2060", "Placa de video con 6GB de VRAM GDDR6", 39999, "gpu", "media/productos/gpus/rtx-2060.jpg"),
+    new Producto(11, "Asus", "RTX 3080", "Placa de video con 12GB de VRAM GDDR6X", 125999, "gpu", "media/productos/gpus/rtx-3080.png"),
+    new Producto(12, "Gigabyte", "RX 5700 XT", "Placa de video con 8GB de VRAM GDDR6", 99999, "gpu", "media/productos/gpus/rx-5700-xt.png")
+];
 
-// let listadoProductos = document.getElementById("listadoProductos");
 
-// let nuevoProducto = document.createElement("p");
-// nuevoProducto.classList.add("col", "mb-4");
-// nuevoProducto.innerHTML =   `<div class="card h-100 intel">
-//                             <img src="media/productos/procesadores/intel-i3-10th.jpg" class="card-img-top" alt="...">
-//                                 <div class="card-body">
-//                                     <h5 class="precio">$14999</h5>
-//                                     <h5 class="card-title">Intel i3 10100</h5>
-//                                     <p class="card-text">Microprocesador de 4 núcleo y 8 hilos</p>
-//                                     <button type="button" class="btn btn-primary btn-lg btn-block">Agregar al carrito</button>
-//                                 </div>
-//                             </div>`
-// listadoProductos.appendChild(nuevoProducto);    
+let listadoProductosRow = document.querySelector("#listadoProductos");
+console.log(listadoProductosRow);
 
+arrProductos.forEach((producto) => {
+    let divCard = document.createElement("div");
+    divCard.classList.add("col", "mb-4");
+    divCard.innerHTML =     `<div class="card h-100 producto">
+                                <img src="${producto.urlFoto}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="precio">$${producto.precio}</h5>
+                                    <h5 class="card-title">${producto.marca} ${producto.modelo}</h5>
+                                    <p class="card-text">${producto.descripcion}</p>
+                                    <div class="inputCantidadContainer">
+                                        <label for="cantidad1">Cantidad</label>
+                                        <input type="number" class="form-control my-2" id="cantidad1" value="1" min="1">
+                                    </div>
+                                    <button id="agregarProducto" type="button" class="btn btn-primary btn-lg btn-block">Agregar al carrito</button>
+                                </div>
+                            </div>`
+    listadoProductosRow.appendChild(divCard)
+    console.log(divCard);
+})
